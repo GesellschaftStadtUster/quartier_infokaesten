04cf7494172366cacb1c605712857c62a324f4e1
Andreas Wyss <78662247+socialthink@users.noreply.github.com>
2021-03-19 23:23:41 +0100

    Add files via upload

---
+++ script.R
@@ -0,0 +1,24 @@
+##Bibliothek
+library(RCurl)
+library(leaflet)
+library(htmlwidgets)
+
+
+#Daten einlesen
+daten <- read.csv("https://raw.githubusercontent.com/KiJuInStadtUster/quartier_infokaesten/main/liste_quartierinfokaesten_20210319.csv")
+
+popuptext <- paste0("<h1>",daten$standort,"</h1>",
+               "<b>Quartierverein:</b> ", daten$quartierverein,
+               "<br><b>Zuständigkeit:</b> ", daten$besitzer,
+               "<br><b>Google Streetview:</b> <a href=\"",daten$link_google.streetview,"\", target=_blank>LINK</a>",
+               "<br><a href=\"",daten$bild_kasten,"\", target=_blank><img src=\"",daten$bild_kasten,"\" alt=\"Bild des Kasten\" width=\"100%\" height=\"100%\"></a>")
+
+farbe <- daten$besitzer
+farbe[farbe=="stadt"] <- "red"
+farbe[farbe=="quartierverein"] <- "green"
+
+m <- leaflet() %>% 
+  addTiles() %>% 
+  addCircleMarkers(lng=daten$lng, lat=daten$lat, popup=popuptext, color = farbe, radius = 15)
+saveWidget(m, file="karte.html")
+m  # Print the map
